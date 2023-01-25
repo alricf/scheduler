42dded45182704f18a0a97b4a8ec6692239a8dd3
ACF <alricfernandes11@gmail.com>
2023-01-25 16:30:28 -0500

    Updating names of props received by DayListItem component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 import DayListItem from 'components/DayListItem';
 
-export default function DayList(props){
+export default function DayList(props) {
   const dayList = props.days.map(day => {
-    return <DayListItem 
-      key={day.id}
-      name={day.name}
-      spots={day.spots}
-      selected={day.name === props.day}
-      setDay={props.setDay}
-    />
-  })
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={props.onChange}
+      />
+    );
+  });
 
   return (
     <ul>
