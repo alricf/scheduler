3b0c0251544c506902819068901335c46b66abf3
ACF <alricfernandes11@gmail.com>
2023-01-25 12:17:18 -0500

    Conditionally adding classes for the list element in the component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,9 +2,16 @@ import React from "react";
 
 import "components/InterviewerListItem.scss";
 
+import classNames from "classnames";
+
 export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item" onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
