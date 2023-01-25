334b850cf46202ecccb6de6c0eb05bb956ef361c
ACF <alricfernandes11@gmail.com>
2023-01-25 11:59:54 -0500

    Replacing hardcoded values with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
@@ -5,10 +7,10 @@ export default function InterviewerListItem(props) {
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 };
