f38243171b3846dd22032261b9dda5610e843560
ACF <alricfernandes11@gmail.com>
2023-01-26 15:45:56 -0500

    Implementing show story

--- stories/index.js
+++ stories/index.js
@@ -152,4 +152,12 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")} 
+    />
+  ))
     
