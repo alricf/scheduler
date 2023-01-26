19ed5d8c3c137e7b216107694cd5b6ac9c8c3dd8
ACF <alricfernandes11@gmail.com>
2023-01-26 12:05:31 -0500

    Adding a story

--- stories/index.js
+++ stories/index.js
@@ -146,3 +146,4 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
