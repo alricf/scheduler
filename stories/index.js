58d6c4d81d8bfa612d8806ef472185e36b54ce1c
ACF <alricfernandes11@gmail.com>
2023-01-26 12:48:02 -0500

    Adding a storybook action

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // Button Stories
 storiesOf("Button", module)
@@ -148,5 +149,6 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
     
