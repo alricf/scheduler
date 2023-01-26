bfd6841435cd33c0bf9620d83e820d00b03839f0
ACF <alricfernandes11@gmail.com>
2023-01-26 12:24:23 -0500

    Adding header story

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
 
 // Button Stories
 storiesOf("Button", module)
@@ -147,3 +148,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Header", () => <Header time="12pm" />);
+    
