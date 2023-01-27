4db7159f34f431e24aa4f3cc4ab98d92f38e4574
ACF <alricfernandes11@gmail.com>
2023-01-27 11:41:58 -0500

    Adding stories

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 // Button Stories
 storiesOf("Button", module)
@@ -173,7 +174,23 @@ storiesOf("Appointment", module)
   .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => (
     <Error
-    message="Could not delete appointment."
-    onClose={action("onClose")}
+      message="Could not delete appointment."
+      onClose={action("onClose")}
     />
   ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      student="Alric Charles Fernandes"
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ));
