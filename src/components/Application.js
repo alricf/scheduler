3e6ae28d3a40ff598d78f2ddfe748b09ffae868c
ACF <alricfernandes11@gmail.com>
2023-01-27 17:00:38 -0500

    Spreading props

--- src/components/Application.js
+++ src/components/Application.js
@@ -69,9 +69,7 @@ export default function Application(props) {
     return (
       <Appointment
         key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={appointment.interview}
+        {...appointment}
       />
     );
   })
