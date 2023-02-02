9c5e1006c698b75e83b452382107b776863a2497
ACF <alricfernandes11@gmail.com>
2023-02-02 17:22:51 -0500

    Fixing bugs and setting back limit

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -8,13 +8,22 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   // Transition mode
-  function transition(mode) {
-    setMode(mode);
+  function transition(newMode) {
+    setMode(newMode);
+    setHistory([...history, newMode]);
   }
 
   // Back mode
-  function Back(){
 
+  function back() {
+    if (history.length > 1) {
+      let oldHistory = [...history];
+      console.log(oldHistory, "old");
+      oldHistory.pop();
+      console.log(oldHistory, "new");
+      setMode(oldHistory[oldHistory.length - 1]);
+      setHistory(oldHistory);
+    }
   }
 
   return { mode, transition, back };
