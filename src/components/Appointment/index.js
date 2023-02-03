c65ebfa0ee41b20d913e99004f49d5a08090e69a
ACF <alricfernandes11@gmail.com>
2023-02-03 12:00:41 -0500

    Implementing conditional rendering

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,11 +6,29 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
+
+  // Hooks
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  // Template
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 }
