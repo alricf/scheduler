9aef5064745d1ed3871d066e3b0795a824deefc9
ACF <alricfernandes11@gmail.com>
2023-02-17 15:16:08 -0500

    Updating test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -18,22 +18,21 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
-
+  
     await waitForElement(() => getByText(container, "Archie Cohen"));
-
+  
     const appointments = getAllByTestId(container, "appointment");
-
-    const appointment = getAllByTestId(container, "appointment")[0];
-
+    const appointment = appointments[0];
+  
     fireEvent.click(getByAltText(appointment, "Add"));
-
+  
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
+  
     fireEvent.click(getByText(appointment, "Save"));
-
+  
     console.log(prettyDOM(appointment));
   });
 });
