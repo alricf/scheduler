ddbcda579e96e932b1ba71b405f450f532f02828
ACF <alricfernandes11@gmail.com>
2023-02-21 12:41:38 -0500

    Completing test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -27,7 +27,7 @@ describe("Appointments", () => {
     // Click save button in the form.
     cy.contains("Save").click();
 
-    // Verify that new appointment has been booked.
+    // Verify that new interview has been booked.
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
@@ -38,23 +38,38 @@ describe("Appointments", () => {
     cy.get("[alt=Edit]")
       .first()
       .click({ force: true });
-  
+
     // Edit student name in the form.
     cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
 
     // Edit interviewer in the form.
     cy.get("[alt='Tori Malcolm']").click();
-  
+
     // Click save button in the form.
     cy.contains("Save").click();
-  
-    // Verify if edited appointment has been booked.
+
+    // Verify if edited interview has been booked.
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
   // Test 3
   it("should cancel an interview", () => {
-    
-  })
+    // Click delete on booked interview.
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+  
+    // Click confirm.
+    cy.contains("Confirm").click();
+  
+    // Check if "Deleting" status appears.
+    cy.contains("Deleting").should("exist");
+
+    // Check if "Deleting" status disappears.
+    cy.contains("Deleting").should("not.exist");
+  
+    // Verify original booked interview has been deleted.
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
 });
