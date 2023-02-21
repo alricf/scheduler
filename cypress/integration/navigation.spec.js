c357c26b7d57e476efef9073714510e56f0517f8
ACF <alricfernandes11@gmail.com>
2023-02-21 17:20:04 -0500

    Adding comments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,8 +5,10 @@ describe("Navigation", () => {
   });
 
   it("should navigate to Tuesday", () => {
+    // Loads page.
     cy.visit("/");
-  
+    
+    // Clicks Tuesday and verifies that it is selected.
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected")
